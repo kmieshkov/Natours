eb7b825b7ad253fc7eaaa7528e41a222c871d9da
Kyrylo Mieshkov <mieshkov.k@gmail.com>
2025-01-21 23:42:49 -0500

    Quick script to populate data in DB from local file

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('../../models/tourModel');
+
+dotenv.config({ path: `./config.env` });
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB connection successful!');
+  });
+
+// READ JSON FILE
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'));
+
+// IMPORT DATA INTO DATABASE
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded!');
+    mongoose.disconnect();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// DELETE ALL DATA FROM COLLECTION
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfully deleted!');
+    mongoose.disconnect();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+(async () => {
+  try {
+    if (process.argv[2] === '--import') {
+      await importData();
+    } else if (process.argv[2] === '--delete') {
+      await deleteData();
+    } else {
+      console.log('You must specify --import or -delete');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+})();
