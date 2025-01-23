588daf96624a735e1ea6af587a37b73472120a88
Kyrylo Mieshkov <mieshkov.k@gmail.com>
2025-01-22 23:27:17 -0500

    Refactor API features

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,5 @@
 const Tour = require('../models/tourModel');
+const APIFeatures = require('../utils/apiFeatures');
 
 exports.checkBody = (req, res, next) => {
   // Middleware for specific route
@@ -17,50 +18,13 @@ exports.aliasTopTours = (req, res, next) => {
 
 exports.getAllTours = async (req, res) => {
   try {
-    // 1. Build query
-    // 1a. Filtering
-    const { sort, ...queryObj } = req.query;
-    const excludedFields = ['page', 'limit', 'fields'];
-    excludedFields.forEach((el) => delete queryObj[el]);
-
-    // 1b. Advanced filtering
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-
-    // 1c. Init query
-    const query = Tour.find(JSON.parse(queryStr));
-
-    // 2. Sorting
-    if (sort) {
-      const sortBy = sort.split(',').join(' ');
-      query.sort(sortBy);
-    } else {
-      query.sort('-createdAt'); // If sorting not specified - sort by created date
-    }
-
-    // 3. Fileds limiting
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query.select(fields);
-    } else {
-      query.select('-__v'); // Exclude __v by default
-    }
-
-    // 4. Pagination
-    const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 100;
-    const skip = (page - 1) * limit;
-    query.skip(skip).limit(limit);
-
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) {
-        throw new Error('This page does not exist');
-      }
-    }
-
     // Execute query
-    const tours = await query;
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
 
     // Send response
     res.json({
