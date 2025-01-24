575a2a27337dce3ae16982e6c70be6226b2d3e07
Kyrylo Mieshkov <mieshkov.k@gmail.com>
2025-01-24 17:15:05 -0500

    Implement dev vs prod  error handling

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,10 +1,37 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // If error is operational (confirmed erorr) - send details
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.error('ERROR 💥', err);
+
+    // Otherwise (unnown erorr) - send generic error to not leak any details
+    res.status(500).json({
+      status: 'error',
+      message: 'Oooops... Something went wrong!',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.StatusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.statusCode,
-    message: err.message,
-  });
-  next();
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
