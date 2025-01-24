429b39793f4239b6da900d9c62b6165afa5d1f0b
Kyrylo Mieshkov <mieshkov.k@gmail.com>
2025-01-24 18:12:23 -0500

    Error handler for unhandled rejections

--- server.js
+++ server.js
@@ -22,3 +22,10 @@ const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
+
+// Global error handling for Unhandled Promise Rejections
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHADNDLED REJECTION! 💥 Shutting down...');
+  process.exit(1);
+});
