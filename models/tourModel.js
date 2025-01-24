ce4d7821f9c69c8379aa2a8c88384d66a3084e78
Kyrylo Mieshkov <mieshkov.k@gmail.com>
2025-01-23 21:48:55 -0500

    Add database (document, query, aggregation) middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -7,6 +8,7 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name'],
       unique: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration'],
@@ -51,6 +53,10 @@ const tourSchema = new mongoose.Schema(
       default: Date.now(),
     },
     startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     toJSON: { virtuals: true },
@@ -62,6 +68,46 @@ const tourSchema = new mongoose.Schema(
 // that is not stored in the database and cannot be used for filtering or querying
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
+});
+
+/********* Document middleware *********/
+
+// Runs before .save() and .create(),
+// but does not work with insertMany() or findOneAndUpdate().
+// this keyword points to the current document
+tourSchema.pre('save', function (next) {
+  console.log('Will create slug...');
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+tourSchema.post('save', (doc, next) => {
+  console.log('After save document middleware...');
+  next();
+});
+
+/********* Query middleware *********/
+
+// this keyword points to current query
+// because we're not processing documents, we're processing query
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`Query took ${Date.now() - this.start} ms`);
+  next();
+});
+
+/********* Aggregation middleware *********/
+
+// this keyword points to current aggregation object
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  console.log(this.pipeline());
+  next();
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
