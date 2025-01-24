f821492de417187c1f1b17bd1ed9a8baeea62a81
Kyrylo Mieshkov <mieshkov.k@gmail.com>
2025-01-24 18:38:28 -0500

    Error handler for uncaught exceptions

--- server.js
+++ server.js
@@ -23,10 +23,23 @@ const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
 
-// Global error handling for Unhandled Promise Rejections
+// Global error handling for Unhandled Promise Rejections and Uncaught Exceptions
+
+// Unhandled rejections: Shutting down is recommended to ensure reliability and catch missed errors
 process.on('unhandledRejection', (err) => {
-  console.log(err.name, err.message);
   console.log('UNHADNDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+
+  // Allow server to finish handling pending requests before shutting down
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+// Uncaught exceptions: Must shut down the app to avoid running in a corrupted state
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
 
   // Allow server to finish handling pending requests before shutting down
   server.close(() => {
