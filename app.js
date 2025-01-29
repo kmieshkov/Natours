5081133b7a0c687fece0ec2536e3b32e0446734d
Kyrylo Mieshkov <mieshkov.k@gmail.com>
2025-01-28 21:42:53 -0500

    Add rate limiting

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -13,6 +14,16 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+// Allow N requests (100) from the same IP in M milliseconds (1 hour)
+// Resets after server restarts
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP. Please try again in an hour!',
+});
+
+app.use('/api', limiter);
 
 app.use(express.json()); // Modify incoming data
 app.use(express.static(`${__dirname}/public`)); // Serve static files
