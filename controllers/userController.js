764be3960580ec34bdf7a47d894e8469d7369f85
Kyrylo Mieshkov <mieshkov.k@gmail.com>
2025-02-03 18:34:10 -0500

    API to get current user

--- controllers/userController.js
+++ controllers/userController.js
@@ -59,6 +59,11 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
   });
 });
 
+exports.getMe = (req, res, next) => {
+  req.params.id = req.user.id;
+  next();
+};
+
 exports.getAllUsers = factory.getAll(User);
 exports.getUser = factory.getOne(User);
 exports.updateUser = factory.updateOne(User); // Do NOT update password with this
