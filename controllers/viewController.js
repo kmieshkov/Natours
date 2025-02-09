41341e0b6ae50b4ea35e6db89652bcb4e7b995b9
Kyrylo Mieshkov <mieshkov.k@gmail.com>
2025-02-09 18:01:09 -0500

    Infrastructure for server side rendering views

---
+++ controllers/viewController.js
@@ -0,0 +1,15 @@
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // 1. Get tour data from collection
+  const tours = await Tour.find();
+
+  // 2. Build template - done in /views/overview.pug
+  // 3. Render template using data from step #1
+
+  res.status(200).render('overview', {
+    title: 'All tours',
+    tours,
+  });
+});
