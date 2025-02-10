13a77ec0045c7288361c79fa6f10fd1cf8cddb7f
Kyrylo Mieshkov <mieshkov.k@gmail.com>
2025-02-09 22:55:33 -0500

    Add tour pug template

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -13,3 +13,19 @@ exports.getOverview = catchAsync(async (req, res, next) => {
     tours,
   });
 });
+
+exports.getTour = catchAsync(async (req, res, next) => {
+  // 1. Get data for the requested tour (including reviews and guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fileds: 'review rating user',
+  });
+
+  // 2. Build template - done it /views/tour.pug
+  // 3. Render template using data from step #1
+
+  res.status(200).render('tour', {
+    title: tour.name,
+    tour,
+  });
+});
