c89a72fd3dac10ac40edfc4eb64c858cb402b605
Kyrylo Mieshkov <mieshkov.k@gmail.com>
2025-02-13 23:37:28 -0500

    User photo upload with multer

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,34 @@
+const multer = require('multer');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 const factory = require('./handlerFactory');
+
+const multerStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/img/users');
+  },
+  filename: (req, file, cb) => {
+    const ext = file.mimetype.split('/')[1];
+    cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+  },
+});
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Not an image! Please upload only images.', 400), false);
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+
+// Handles a single image upload; 'photo' is the form field name
+exports.uploadUserPhoto = upload.single('photo');
 
 // Filters an object to include only specified fields
 const filterObj = (obj, ...allowedFields) => {
@@ -35,6 +62,9 @@ exports.updateMe = catchAsync(async (req, res, next) => {
 
   // 2. Filtered out unwanted properties that are not allowed to be updated
   const filteredBody = filterObj(req.body, 'name', 'email');
+  if (req.file) {
+    filteredBody.photo = req.file.filename; // File name from multer
+  }
 
   // 3. Update user document
   const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
