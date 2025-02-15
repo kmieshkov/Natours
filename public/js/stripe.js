20ac19962996cacab4490ab8acda4d11a92fa4d8
Kyrylo Mieshkov <mieshkov.k@gmail.com>
2025-02-14 23:02:58 -0500

    Fix stripe issue

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -3,12 +3,12 @@
 import axios from 'axios';
 import { showAlerts } from './alerts';
 
-const stripe = Stripe(
-  'pk_test_51QsaaPF7zjmfUJG9UpvVJWBxrzXMi8ZzA12yziFa5x4UVehJhIrO6lcJh32JeoGpeS8qjvuz9zJ8ayqchB5FEPhr00tzbjHin7',
-);
-
 export const bookTour = async (tourId) => {
   try {
+    const stripe = Stripe(
+      'pk_test_51QsaaPF7zjmfUJG9UpvVJWBxrzXMi8ZzA12yziFa5x4UVehJhIrO6lcJh32JeoGpeS8qjvuz9zJ8ayqchB5FEPhr00tzbjHin7',
+    );
+
     // 1. Get checkout session from API
     const session = await axios(
       `/api/v1/bookings/checkout-session/${tourId}?prefilled_email=jenny%40example.com`,
