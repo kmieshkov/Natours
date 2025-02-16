76c448aca466f19a216a31f49decff4541627365
Kyrylo Mieshkov <mieshkov.k@gmail.com>
2025-02-16 17:40:44 -0500

    FIx bug with empty tour title

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -84,7 +84,7 @@ exports.getMyTours = catchAsync(async (req, res, next) => {
   const tours = await Tour.find({ _id: { $in: tourIDs } });
 
   res.status(200).render('overview', {
-    status: 'success',
+    title: 'My Tours',
     tours,
   });
 });
