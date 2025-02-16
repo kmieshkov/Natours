0edb6163bb91ae66a5fdaa8882ddcefd81acfb1e
Kyrylo Mieshkov <mieshkov.k@gmail.com>
2025-02-16 14:30:02 -0500

    Implement CORS

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
 const nocache = require('nocache');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -25,6 +26,10 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 /********* Middleware *********/
+
+// Implement CORS
+app.use(cors());
+app.options('*', cors());
 
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
